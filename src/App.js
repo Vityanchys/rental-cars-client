75e994118ef5b6e9e6065183a73fed99885b2ef6
Mikutsevich Anton <mikutsevich.anton@gmail.com>
2017-04-14 13:47:19 +0300

    Add vehicles component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Vehicle from './components/Vehicle';
+import Vehicles from './components/Vehicles';
+
+var vehicles = [
+  {
+    name: "Mini Cooper",
+    cost: "$100",
+    id: 1
+  },
+  {
+    name: "Mini Cooper2",
+    cost: "$200",
+    id: 2
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Vehicle name="Mini Cooper" cost="$100" id="1" />
+        <Vehicles vehicles={vehicles} />
       </div>
     );
   }
---
+++ src/components/Vehicles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Vehicle from "./Vehicle";
+
+
+function Vehicles(props) {
+  return (
+  <div className="vehicles"> {
+    props.vehicles.map ((vehicle) => {
+      return <Vehicle key={vehicle.id} name={vehicle.name} cost={vehicle.cost} id={vehicle.id} />
+    })}
+  </div>
+  );
+}
+
+export default Vehicles;
