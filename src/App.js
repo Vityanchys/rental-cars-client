34189f8776efca1c8b8217de43fb1348ec695155
Mikutsevich Anton <mikutsevich.anton@gmail.com>
2017-04-14 13:33:17 +0300

    Add vehicle component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Vehicle from './components/Vehicle';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Vehicle name="Mini Cooper" cost="$100" id="1" />
       </div>
     );
   }
---
+++ src/components/Vehicle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Vehicle(props) {
+  return (
+    <div className="vehicle">
+      <h1>{props.name}</h1>
+      Стоимость: {props.cost}
+      <a href={"/vehicles/"+props.id}>Подробнее</a>
+    </div>
+  );
+}
+
+export default Vehicle;
