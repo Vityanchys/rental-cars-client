2f7e4b3fb69f7fb452ca6601ae68df10cf0e93d8
Mikutsevich Anton <mikutsevich.anton@gmail.com>
2017-04-14 13:25:42 +0300

    Add navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="/">Главная</a>
+      <a href="/signup">Регистрация</a>
+    </div>
+  );
+}
+
+export default Navbar;
