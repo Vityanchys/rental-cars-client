7a10fe10eaffd02a2a97bf67e68b7678a9bafb68
mick842 <mikutsevich.anton@gmail.com>
2017-04-21 14:56:28 +0300

    Add login form

---
+++ src/components/LogInForm.js
@@ -0,0 +1,56 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+const LogInForm = ({
+  onSubmit,
+  onChange,
+  errors,
+  user
+}) => (
+  <Card className="container">
+    <form action="/" onSubmit={onSubmit}>
+      <h2 className="card-heading">Login</h2>
+
+      {errors.summary && <p className="error-message">{errors.summary}</p>}
+
+      <div className="field-line">
+        <TextField
+          floatingLabelText="Email"
+          name="email"
+          errorText={errors.email}
+          onChange={onChange}
+          value={user.email}
+        />
+      </div>
+
+      <div className="field-line">
+        <TextField
+          floatingLabelText="Пароль"
+          type="password"
+          name="password"
+          onChange={onChange}
+          errorText={errors.password}
+          value={user.password}
+        />
+      </div>
+
+      <div className="button-line">
+        <RaisedButton type="submit" label="Войти" primary />
+      </div>
+
+      <CardText>Ещё не зарегистрированны? <Link to={'/signup'}>Зарегистрироваться</Link>.</CardText>
+    </form>
+  </Card>
+);
+
+LogInForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired
+};
+
+export default LogInForm;
