87d8318277dc20531e475c1caa3fbd2b0ceca2fd
mick842 <mikutsevich.anton@gmail.com>
2017-04-21 17:39:12 +0300

    Update vehicles view

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -6,7 +6,7 @@ function Vehicles(props) {
   return (
   <div className="vehicles"> {
     props.vehicles.map ((vehicle) => {
-      return <Vehicle key={vehicle.id} name={vehicle.name} cost={vehicle.cost} id={vehicle.id} />
+      return <Vehicle key={vehicle.id} image={vehicle.image} name={vehicle.name} cost={vehicle.cost} id={vehicle.id} />
     })}
   </div>
   );
