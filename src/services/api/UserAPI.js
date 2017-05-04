7155b51c7b0659ae8e95a6f1716f1f481444b2a6
mick842 <mikutsevich.anton@gmail.com>
2017-05-04 21:59:11 +0300

    Fix interation with server

--- src/services/api/UserAPI.js
+++ src/services/api/UserAPI.js
@@ -1,30 +1,21 @@
-function handeResponse(response) {
-    if (response.statusText !== "OK") {
-        throw new Error('Looks like there was a problem. Status Code: ' +
-            response.status);
-        return 'Looks like there was a problem. Status Code: ' + response.status;
-    }
-    else {
-        return 'Registration successful';
-    }
-}
+import { ServerURL } from '../../constants/Constants';
 
 exports.register = async (user) => {
-    const response = await fetch('user/register', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            password: user.password,
-            passportId: user.passportId,
-            admin: false,
-            phone: user.phone,
-        })
+  const response = await fetch(ServerURL + 'user/register', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      password: user.password,
+      passportId: user.passportId,
+      admin: false,
+      phone: user.phone,
     })
-    await handeResponse(response);
+  })
+  return response;
 }
