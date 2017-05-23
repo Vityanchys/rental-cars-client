125af299050437c16c42a5ff3b5f086d5f5a02e9
mick842 <mikutsevich.anton@gmail.com>
2017-05-23 21:40:39 +0300

    Implement authorization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import User from '../modules/User';
 
-function Navbar() {
-  return (
-    <div className="navbar">
-      <div className="navbar-left">
-        <Link to="/">Главная</Link>
+class Navbar extends Component {
+  render() {
+    console.log(this.props.authorized)
+    if (this.props.authorized) {
+      const user = User.get();
+      return (
+        <div className="navbar">
+          <div className="navbar-left">
+            <Link to="/">Главная</Link>
+          </div>
+          <div className="navbar-right">
+            {user.admin && <Link to="/add">Добавить Т/С</Link>}
+            <Link to="/profile">{user.firstName} (ЛК)</Link>
+            <Link to="/logout">Выход</Link>
+          </div>
+        </div>
+      )
+    }
+    return (
+      <div className="navbar" >
+        <div className="navbar-left">
+          <Link to="/">Главная</Link>
+        </div>
+        <div className="navbar-right">
+          <Link to="/login">Вход</Link>
+          <Link to="/signup">Регистрация</Link>
+        </div>
       </div>
-      <div className="navbar-right">
-        <Link to="/profile">Личный кабинет</Link>
-        <Link to="/add">Добавить Т/С</Link>
-        <Link to="/login">Вход</Link>
-        <Link to="/signup">Регистрация</Link>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Navbar;
