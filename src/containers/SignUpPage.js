757c374ecc19255929ae6e91a4c78c568136e035
Oleg <andoider87@gmail.com>
2017-05-26 14:55:00 +0300

    Display email exists message, redirect to login page if registration completed, removed proxy from package.json

--- src/containers/SignUpPage.js
+++ src/containers/SignUpPage.js
@@ -1,72 +1,85 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SignUpForm from '../components/SignUpForm.js';
+import { Redirect } from 'react-router-dom';
 import UserAPI from "../services/api/UserAPI";
 import Validate from "../services/Validate";
 
 class SignUpPage extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      errors: {},
-      user: {
-        email: '',
-        firstName: '',
-        lastName: '',
-        password: '',
-        passportId: '',
-        phone: '',
-        phonePrefix: ''
-      }
-    };
+        this.state = {
+            errors: {},
+            user: {
+                email: '',
+                firstName: '',
+                lastName: '',
+                password: '',
+                passportId: '',
+                phone: '',
+                phonePrefix: ''
+            },
+            complete: false
+        };
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChange = this.onChange.bind(this);
-  }
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
 
-  onChange(event) {
-    const field = event.target.name;
-    const user = this.state.user;
-    user[field] = event.target.value;
+    onChange(event) {
+        const field = event.target.name;
+        const user = this.state.user;
+        user[field] = event.target.value;
 
-    this.setState({
-      user
-    });
-  }
-
-  //@param {object} event - the JavaScript event object
-  onSubmit(event) {
-    event.preventDefault();
-
-    this.setState({
-      errors: {}
-    });
-    
-    Validate.checkUser(this.state.user).then(result => {
-      if (result.success) {
-        UserAPI.register(this.state.user).then(response => {
-          if (response.status === 200) {
-            alert('Registration complete');
-          }
-        })
-      } else {
         this.setState({
-          errors: result.errors
+            user
         });
-      }
-    })
-  }
+    }
 
-  render() {
-    return (
-      <SignUpForm
-        onSubmit={this.onSubmit}
-        onChange={this.onChange}
-        errors={this.state.errors}
-        user={this.state.user}
-      />
-    );
-  }
+    //@param {object} event - the JavaScript event object
+    async onSubmit(event) {
+        event.preventDefault();
+
+        const user = this.state.user;
+
+        const checkResult = await Validate.checkUser(user);
+        if (!checkResult.success) {
+            this.setState({errors: checkResult.errors});
+            return;
+        }
+
+        let response = await UserAPI.register(user);
+
+        if (response.status === 200) {
+            this.setState({
+                errors: {},
+                complete: true,
+            });
+        } else {
+            let errors = {};
+            response.json().then(json => {
+                errors.summary = json.errors;
+
+                this.setState({
+                    errors: errors
+                });
+            });
+        }
+    }
+
+    render() {
+        if (this.state.complete) {
+            return <Redirect to="/login" />
+        }
+        return (
+            <SignUpForm
+                onSubmit={this.onSubmit}
+                onChange={this.onChange}
+                errors={this.state.errors}
+                user={this.state.user}
+            />
+        );
+    }
 }
 
 export default SignUpPage;
