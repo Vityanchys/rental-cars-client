b95072edf3a584cffc488a77218bf0e16d7275a0
mick842 <mikutsevich.anton@gmail.com>
2017-06-18 01:05:05 +0300

    Fix vehicle page style

--- src/components/VehiclePage.js
+++ src/components/VehiclePage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table';
-import { Card } from 'material-ui/Card';
+import { Card, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import User from '../modules/User';
 
 const VehiclePage = ({
   vehicle,
@@ -10,74 +11,80 @@ const VehiclePage = ({
   onDelete,
 }) => (
 
-    <Card className="vehicleInfo">
-      <h1>Информация о Т/С</h1>
-      <RaisedButton
-        onClick={onDelete}
-        label="Удалить Т/С"
-        secondary={true}
-        style={{ float: 'right' }}
-      />
+    <Card className="card-vehicle-page">
+      <CardText>
+        <h1>Информация о Т/С</h1>
 
-      <div className='imgVehicleTable'>
-        <img alt="img" src={vehicle.image} width="100%" />
-      </div>
-      <div className='vehicleTable'>
-        <Table>
-          <TableBody
-            displayRowCheckbox={false}
-          >
-            <TableRow>
-              <TableRowColumn>Mark:</TableRowColumn>
-              <TableRowColumn>{vehicle.mark}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Model:</TableRowColumn>
-              <TableRowColumn>{vehicle.model}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Year:</TableRowColumn>
-              <TableRowColumn>{vehicle.year}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Gearbox type:</TableRowColumn>
-              <TableRowColumn>{vehicle.gearboxType}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Engine volume:</TableRowColumn>
-              <TableRowColumn>{vehicle.engineVolume}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Body type:</TableRowColumn>
-              <TableRowColumn>{vehicle.bodyType}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Capacity:</TableRowColumn>
-              <TableRowColumn>{vehicle.capacity}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Carrying capacity:</TableRowColumn>
-              <TableRowColumn>{vehicle.carryingCapacity}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Price per hour:</TableRowColumn>
-              <TableRowColumn>{vehicle.pricePerHour}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Price per day:</TableRowColumn>
-              <TableRowColumn>{vehicle.pricePerDay}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Type:</TableRowColumn>
-              <TableRowColumn>{vehicle.type}</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>Status:</TableRowColumn>
-              <TableRowColumn>{vehicle.status}</TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+        <div style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "center"}}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <img alt="img" src={vehicle.image} style={{ maxWidth: "600px", width: "100%" }} />
+            {User.get() && User.get().admin &&
+              <RaisedButton
+                onClick={onDelete}
+                label="Удалить Т/С"
+                secondary={true}
+                style={{ float: 'right' }}
+              />
+            }
+          </div>
+          <div style={{ maxWidth: "600px" }}>
+            <Table>
+              <TableBody
+                displayRowCheckbox={false}
+              >
+                <TableRow>
+                  <TableRowColumn>Mark:</TableRowColumn>
+                  <TableRowColumn>{vehicle.mark}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Model:</TableRowColumn>
+                  <TableRowColumn>{vehicle.model}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Year:</TableRowColumn>
+                  <TableRowColumn>{vehicle.year}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Gearbox type:</TableRowColumn>
+                  <TableRowColumn>{vehicle.gearboxType}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Engine volume:</TableRowColumn>
+                  <TableRowColumn>{vehicle.engineVolume}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Body type:</TableRowColumn>
+                  <TableRowColumn>{vehicle.bodyType}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Capacity:</TableRowColumn>
+                  <TableRowColumn>{vehicle.capacity}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Carrying capacity:</TableRowColumn>
+                  <TableRowColumn>{vehicle.carryingCapacity}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Price per hour:</TableRowColumn>
+                  <TableRowColumn>{vehicle.pricePerHour}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Price per day:</TableRowColumn>
+                  <TableRowColumn>{vehicle.pricePerDay}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Type:</TableRowColumn>
+                  <TableRowColumn>{vehicle.type}</TableRowColumn>
+                </TableRow>
+                <TableRow>
+                  <TableRowColumn>Status:</TableRowColumn>
+                  <TableRowColumn>{vehicle.status}</TableRowColumn>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </CardText>
     </Card>
   );
 
