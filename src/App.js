85b6607ae241f013f2fe8350552b33dded04fc5a
mick842 <mikutsevich.anton@gmail.com>
2017-06-17 20:48:25 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import HomePage from './containers/HomePage';
 import VehicleRoute from './containers/VehicleRoute';
 import SignUpPage from './containers/SignUpPage';
@@ -68,67 +69,72 @@ class App extends Component {
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <Router>
           <div className="App">
-            <Snackbar
-              open={this.state.snackbar}
-              message={this.state.snackbarMessage}
-              autoHideDuration={4000}
-              onRequestClose={this.handleRequestCloseSnackbar}
-            />
-            <Navbar user={user} />
-            <Switch>
-              <Route exact path="/" render={(props) => (
-                <HomePage {...props} onMessage={this.handleSnackbarMessage} />
-              )} />
-              {
-                user &&
-                <Switch>
-                  <Route path="/cars/:id/order" render={(props) => (
-                    <OrderPage {...props} onMessage={this.handleSnackbarMessage} />
-                  )} />
-                  <Route path="/car/cars/:id" component={VehicleRoute} />
-                  <Route path="/user/users/:id" component={UserRoute} />
-                  <Route path="/profile" component={ProfilePage} />
-                  <Route path='/user/users' component={UsersRoute} />
-                  <Route path='/editUserInformation' render={(props) => (
-                    <EditProfileRoute {...props} onUserUpdate={this.handleUserUpdate} />
-                  )} />
-                  <Route path="/logout" render={() => (
-                    <div>
-                      {this.deauthenticate()}
-                      < Redirect to="/" />
-                    </div>
-                  )} />
-                  {
-                    user.admin && <Route path="/add" render={() => (
-                      <VehicleAddPage onMessage={this.handleSnackbarMessage} />
+            <div className="content">
+              <Snackbar
+                open={this.state.snackbar}
+                message={this.state.snackbarMessage}
+                autoHideDuration={4000}
+                onRequestClose={this.handleRequestCloseSnackbar}
+              />
+              <Navbar user={user} />
+              <Switch>
+                <Route exact path="/" render={(props) => (
+                  <HomePage {...props} onMessage={this.handleSnackbarMessage} />
+                )} />
+                {
+                  user &&
+                  <Switch>
+                    <Route path="/cars/:id/order" render={(props) => (
+                      <OrderPage {...props} onMessage={this.handleSnackbarMessage} />
                     )} />
-                  }
-                  <Redirect from="/login" to="/" />
-                  <Redirect from="/signup" to="/" />
-                </Switch>
-              }
+                    <Route path="/car/cars/:id" component={VehicleRoute} />
+                    <Route path="/user/users/:id" component={UserRoute} />
+                    <Route path="/profile" component={ProfilePage} />
+                    <Route path='/user/users' component={UsersRoute} />
+                    <Route path='/editUserInformation' render={(props) => (
+                      <EditProfileRoute {...props} onUserUpdate={this.handleUserUpdate} />
+                    )} />
+                    <Route path="/logout" render={() => (
+                      <div>
+                        {this.deauthenticate()}
+                        < Redirect to="/" />
+                      </div>
+                    )} />
+                    {
+                      user.admin && <Route path="/add" render={() => (
+                        <VehicleAddPage onMessage={this.handleSnackbarMessage} />
+                      )} />
+                    }
+                    <Redirect from="/login" to="/" />
+                    <Redirect from="/signup" to="/" />
+                  </Switch>
+                }
 
-              <Route path="/car/cars/:id" component={VehicleRoute} />
-              <Route path="/signup" render={() => (
-                <SignUpPage onMessage={this.handleSnackbarMessage} />
-              )} />
-              <Route path="/login" render={() => (
-                <LogInPage onLoggedIn={this.onLoggedIn} />
-              )} />
+                <Route path="/car/cars/:id" component={VehicleRoute} />
+                <Route path="/signup" render={() => (
+                  <SignUpPage onMessage={this.handleSnackbarMessage} />
+                )} />
+                <Route path="/login" render={() => (
+                  <LogInPage onLoggedIn={this.onLoggedIn} />
+                )} />
 
-              <Redirect from="/cars/:id/order" to="/login" />
-              <Redirect from="/profile" to="/login" />
-              <Redirect from="/editUserInformation" to="/login" />
-              <Redirect from="/logout" to="/login" />
-              <Redirect from="/add" to="/login" />
+                <Redirect from="/cars/:id/order" to="/login" />
+                <Redirect from="/profile" to="/login" />
+                <Redirect from="/editUserInformation" to="/login" />
+                <Redirect from="/logout" to="/login" />
+                <Redirect from="/add" to="/login" />
 
-              <Route render={() => (
-                <h1>
-                  You broke the internet (404)
+                <Route render={() => (
+                  <h1>
+                    You broke the internet (404)
               </h1>
-              )} />
+                )} />
 
-            </Switch>
+              </Switch>
+            </div>
+            <div className="footer" >
+              <Footer />
+            </div>
           </div>
         </Router>
       </MuiThemeProvider>
